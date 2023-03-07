64d5731adb8e1fa2deb5d4cbcbcf14970ae6ad9e
matheus <matheussss6613@gmail.com>
2023-03-07 11:28:18 -0300

    Fim

--- src/controller/ThreadVetor.java
+++ src/controller/ThreadVetor.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.Random;
+
 public class ThreadVetor extends Thread
 {
 	private int n;
@@ -16,22 +18,26 @@ public class ThreadVetor extends Thread
 	{
 		
 		this.vet = new int[n];
+		Random r = new Random();
+		
 		double tempoIni = System.nanoTime();
 		
 		if(this.num == 1)
 		{
 			for(int i = 0; i < this.vet.length ; i++)
 			{
-				this.vet[i] = i;
+				this.vet[i] = r.nextInt(99) + 1;
 			}
 			double tempoFim = (System.nanoTime() - tempoIni) / Math.pow(10, 9);
 			System.out.println("Segundos percorrido para o num 1 = " + tempoFim);
 		}
 		else if(this.num == 2)
 		{
+			int i = 0;
 			for(int array : this.vet)
 			{
-				this.vet[array] = array;
+				this.vet[i] = r.nextInt(100) + 1;
+				i++;
 			}
 			double tempoFim = (System.nanoTime() - tempoIni) / Math.pow(10, 9);
 			System.out.println("Segundos percorrido para o num 2 = " + tempoFim);
