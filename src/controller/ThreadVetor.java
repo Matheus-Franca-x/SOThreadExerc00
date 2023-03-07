965290aa6436169c1e6308bdedc0d3dae429758c
matheus <matheussss6613@gmail.com>
2023-03-07 11:14:00 -0300

    Fim

--- src/controller/ThreadVetor.java
+++ src/controller/ThreadVetor.java
@@ -1,5 +1,48 @@
 package controller;
 
-public class ThreadVetor {
+public class ThreadVetor extends Thread
+{
+	private int n;
+	private int num;
+	private int vet[] = new int[n];
+	
+	public ThreadVetor(int num, int n)
+	{
+		this.num = num;
+		this.n = n;;
+	}
+	
+	public void percVetor()
+	{
+		
+		this.vet = new int[n];
+		double tempoIni = System.nanoTime();
+		
+		if(this.num == 1)
+		{
+			for(int i = 0; i < this.vet.length ; i++)
+			{
+				this.vet[i] = i;
+			}
+			double tempoFim = (System.nanoTime() - tempoIni) / Math.pow(10, 9);
+			System.out.println("Segundos percorrido para o num 1 = " + tempoFim);
+		}
+		else if(this.num == 2)
+		{
+			for(int array : this.vet)
+			{
+				this.vet[array] = array;
+			}
+			double tempoFim = (System.nanoTime() - tempoIni) / Math.pow(10, 9);
+			System.out.println("Segundos percorrido para o num 2 = " + tempoFim);
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,18 @@
 package view;
 
+import controller.ThreadVetor;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("test");
+		ThreadVetor thread1 = new ThreadVetor(1, 1000);
+		ThreadVetor thread2 = new ThreadVetor(2, 1000);
+		
+		thread1.percVetor();
+		thread2.percVetor();
+		
+		
+		
 	}
 }
